13ae63aa5c4d0ca309a903687075f909b859d4ea
Masaaki Morishita <ikasama3.0@gmail.com>
2014-09-08 22:55:02 +0900

    Fix Alchemia::Plugins::Base

--- lib/alchemia/plugins/base.rb
+++ lib/alchemia/plugins/base.rb
@@ -2,24 +2,37 @@ class Alchemia
   module Plugins
     class Base
 
+      def initialize(tree)
+        @tree = tree
+      end
+
       def process
         nil
       end
 
-      def selectors(tree)
+      def all_selectors
         selectors = []
-        tree.each do |t|
+        @tree.each do |t|
           if is_rule(t)
-            selectors.push t[:selector][:tokens][1][:raw]
             selectors.push t[:selector][:value]
           end
         end
         selectors
       end
 
-      def properties(tree, selector)
+      def selector(selector)
+        @tree.each do |t|
+          if is_rule(t)
+            if selector == t[:selector][:value]
+              return t[:selector][:tokens][1][:raw]
+            end
+          end
+        end
+      end
+
+      def all_properties(selector)
         properties = []
-        tree.each do |t|
+        @tree.each do |t|
           if is_rule(t)
             t[:children].each do |child|
               if is_property(child)
@@ -40,6 +53,10 @@ class Alchemia
 
       def is_property(node)
         true if node[:property] == :property
+      end
+
+      def is_child(child)
+        true if child[:node] == :property
       end
 
     end
